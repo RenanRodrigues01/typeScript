be771cf66bf5ff2d332ef36c2798c51ea04833a5
RenanRodrigues01 <renan.rodriguesmoura2022@gmail.com>
2024-06-05 10:20:49 -0300

    correction array exercise

--- array/script.ts
+++ array/script.ts
@@ -9,29 +9,64 @@ interface interfaceCursos {
   aulas: number;
   horas: number;
   nome: string;
-  gartuito: boolean;
+  gratuito: boolean;
   tags: Array<string>;
   idAulas: Array<number>;
   nivel: string
 };
 
 fetchCursos();
-
+/* MINHA SOLUÇÃO
 function showCursos(data: Array<interfaceCursos>){
  data.map((item) => 
   { if(item.nivel === "iniciante"){
       document.body.innerHTML += `
       <div>
         <h2 style="color:blue">${item.nome}</h2>
+        <p>Horas: ${curso.horas}</p>
+        <p>Aulas: ${curso.aulas}</p>
+        <p>Tipo: ${curso.gratuito ? 'Gratuito' : 'Pago'}</p>
+        <p>Tags: ${curso.tags.join(', ')}</p>
+        <p>Aulas: ${curso.idAulas.join(' | ')}</p>
       </div>
       `
     }else{
       document.body.innerHTML += `
       <div>
         <h2 style="color:red">${item.nome}</h2>
+        <p>Horas: ${curso.horas}</p>
+        <p>Aulas: ${curso.aulas}</p>
+        <p>Tipo: ${curso.gratuito ? 'Gratuito' : 'Pago'}</p>
+        <p>Tags: ${curso.tags.join(', ')}</p>
+        <p>Aulas: ${curso.idAulas.join(' | ')}</p>
       </div>
       `
     }
   }
 );
-};
+};*/
+
+// SOLUÇÃO DO CURSO
+
+function showCursos(data: Array<interfaceCursos>) {
+  data.forEach( (curso) => {
+    let color;
+
+    if(curso.nivel === 'iniciante'){
+      color = "blue"
+    } else{
+      color = "red"
+    }
+
+    document.body.innerHTML += `
+      <div>
+        <h2 style="color: ${color};">${curso.nome}</h2>
+        <p>Horas: ${curso.horas}</p>
+        <p>Aulas: ${curso.aulas}</p>
+        <p>Tipo: ${curso.gratuito ? 'Gratuito' : 'Pago'}</p>
+        <p>Tags: ${curso.tags.join(', ')}</p>
+        <p>Aulas: ${curso.idAulas.join(' | ')}</p>
+    </div>
+    `
+  });
+}
