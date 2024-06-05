1dc8708cb9f0ac2dcdf14635f537bde9201be561
RenanRodrigues01 <renan.rodriguesmoura2022@gmail.com>
2024-06-05 08:50:45 -0300

    solving types and interfaces exercise

--- types_interfaces/script.js
+++ types_interfaces/script.js
@@ -1,20 +1,44 @@
 "use strict";
-function preencheDados(dado) {
+;
+;
+;
+async function fetchProducts() {
+    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+    const data = await response.json();
+    preencheDados(data);
+}
+fetchProducts();
+function preencheDados(data) {
     document.body.innerHTML += `
     <div>
-      <h2>${dado.marca}</h2>
-      <p>${dado.modelo}</p>
-      <p>Automatico: ${dado.autoamtico ? 'sim' : 'não'} </p>
+      <h2>${data.nome}</h2>
+      <h4>${data.descricao}</h4>
+      <ul>
+        <li>Preço: $${data.preco}</li>
+        <li>Tempo de garantia: ${data.garantia}</li>
+        <li>Seguros contra acidentes: ${data.seguroAcidentes ? "SIM" : "NÃO"}</li>
+      </ul>
+      <h3>Fabricante: ${data.empresaFabricante.nome}</h3>
+      <ul>
+        <li>Pais de origem ${data.empresaFabricante.pais}</li>
+        <li>Ano da fundação ${data.empresaFabricante.fundacao}</li>
+      </ul>
+      <h3>Montadora: ${data.empresaMontadora.nome}</h3>
+      <ul>
+        <li>Pais de origem ${data.empresaMontadora.pais}</li>
+        <li>Ano da fundação ${data.empresaMontadora.fundacao}</li>
+      </ul>
     </div>
   `;
 }
-preencheDados({
-    marca: "Chevrolet",
-    modelo: "Celta",
-    autoamtico: false
+/*preencheDados({
+  marca: "Chevrolet",
+  modelo: "Celta",
+  autoamtico: false
 });
+
 preencheDados({
-    marca: "Volkswagen",
-    modelo: "Gol",
-    autoamtico: false
-});
+  marca: "Volkswagen",
+  modelo: "Gol",
+  autoamtico: false
+});*/ 
--- types_interfaces/script.ts
+++ types_interfaces/script.ts
@@ -2,19 +2,60 @@ type Carro = {
   marca: string;
   modelo: string;
   autoamtico: boolean;
+};
+interface InterfaceProduct {
+  nome: string;
+  preco: number;
+  descricao: string;
+  garantia: string;
+  seguroAcidentes: boolean;
+  empresaFabricante: InterfaceManufacturer;
+  empresaMontadora: InterfaceManufacturer;
+};
+interface InterfaceManufacturer {
+  nome: string;
+  fundacao: number;
+  pais: string;
+};
+interface InterfaceAutomaker {
+  nome: string;
+  fundacao: number;
+  pais: string;
+};
+
+async function fetchProducts() {
+  const response = await fetch('https://api.origamid.dev/json/notebook.json');
+  const data = await response.json()
+  preencheDados(data);
 }
 
-function preencheDados(dado: Carro){
+fetchProducts();
+
+function preencheDados(data: InterfaceProduct){
   document.body.innerHTML +=`
     <div>
-      <h2>${dado.marca}</h2>
-      <p>${dado.modelo}</p>
-      <p>Automatico: ${dado.autoamtico ? 'sim' : 'não' } </p>
+      <h2>${data.nome}</h2>
+      <h4>${data.descricao}</h4>
+      <ul>
+        <li>Preço: $${data.preco}</li>
+        <li>Tempo de garantia: ${data.garantia}</li>
+        <li>Seguros contra acidentes: ${data.seguroAcidentes ? "SIM" : "NÃO" }</li>
+      </ul>
+      <h3>Fabricante: ${data.empresaFabricante.nome}</h3>
+      <ul>
+        <li>Pais de origem ${data.empresaFabricante.pais}</li>
+        <li>Ano da fundação ${data.empresaFabricante.fundacao}</li>
+      </ul>
+      <h3>Montadora: ${data.empresaMontadora.nome}</h3>
+      <ul>
+        <li>Pais de origem ${data.empresaMontadora.pais}</li>
+        <li>Ano da fundação ${data.empresaMontadora.fundacao}</li>
+      </ul>
     </div>
   `
 }
 
-preencheDados({
+/*preencheDados({
   marca: "Chevrolet",
   modelo: "Celta",
   autoamtico: false
@@ -24,4 +65,4 @@ preencheDados({
   marca: "Volkswagen",
   modelo: "Gol",
   autoamtico: false
-});
+});*/
